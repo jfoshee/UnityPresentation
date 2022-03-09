2a189be5571d736c50f297ba03fb6873ed54c68d
Jacob Foshee <jacobf@gmail.com>
2022-03-09 15:49:48 -0600

    chore(Unity): Upgrade to Unity 2022.1.0b11 in order to be able to run on Apple Silicon
    Commented out code in `TutorialCallbacks.cs` that no longer compiles.
    
    Other changes were made automatically.
    
    Logs/Packages-Update.log
    ```
    === Wed Mar  9 15:43:01 2022
    
    Packages were changed.
    Update Mode: updateDependencies
    
    The following packages were updated:
      com.unity.cinemachine from version 2.6.11 to 2.8.4
      com.unity.collab-proxy from version 1.15.12 to 1.15.13
      com.unity.connect.share from version 4.1.1 to 4.2.2
      com.unity.ide.rider from version 2.0.7 to 3.0.12
      com.unity.ide.vscode from version 1.2.4 to 1.2.5
      com.unity.learn.iet-framework from version 1.2.3 to 2.1.1
      com.unity.test-framework from version 1.1.29 to 1.1.31
      com.unity.timeline from version 1.4.8 to 1.7.0-pre.2
    ```

--- Assets/Tutorials/TutorialCallbacks.cs
+++ Assets/Tutorials/TutorialCallbacks.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using Unity.InteractiveTutorials;
+// using Unity.InteractiveTutorials;
 using UnityEditor;
 
 namespace Unity.Tutorials
@@ -15,11 +15,11 @@ namespace Unity.Tutorials
         /// Selects a GameObject in the scene, marking it as the active object for selection
         /// </summary>
         /// <param name="futureObjectReference"></param>
-        public void SelectSpawnedGameObject(FutureObjectReference futureObjectReference)
-        {
-            if (futureObjectReference.sceneObjectReference == null) { return; }
-            SelectGameObject(futureObjectReference.sceneObjectReference.ReferencedObjectAsGameObject);
-        }
+        // public void SelectSpawnedGameObject(FutureObjectReference futureObjectReference)
+        // {
+        //     if (futureObjectReference.sceneObjectReference == null) { return; }
+        //     SelectGameObject(futureObjectReference.sceneObjectReference.ReferencedObjectAsGameObject);
+        // }
 
         public void SelectGameObject(GameObject gameObject)
         {
